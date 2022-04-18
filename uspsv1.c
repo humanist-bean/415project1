f77f8b6126bbfa3eda5149af6f9e8f9944325119
CIS Student <me@debian-BULLSEYE-live-builder-AMD64>
2022-04-18 14:24:07 -0700

    did more stuff, this time using lots of system calls, seems sketchy so I'm pushing again

--- uspsv1.c
+++ uspsv1.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 #include "p1fxns.h"
 
 #define USESTR "usage: [WORKLOAD_FILE] ... \n"
@@ -12,10 +15,13 @@
 int main( UNUSED int argc, char *argv[]){
 	// start by getting processing argv to find workload file
 	// if no workload file is given, process standard input
-	char *fileName;
-	FILE *fp;
+	const char *fileName;
+	int fd;
 	char *args[BUFSIZ];
 	char buf[BUFSIZ];
+	char *string;
+	int status;
+	int index;
 
 	if(argv[1] == NULL){
 		printf("ERROR: You must provide a file name \n");
@@ -25,15 +31,22 @@ int main( UNUSED int argc, char *argv[]){
 	}
 
 	//open file and save words to array
-	fp = fopen(fileName, "r");
-	if(fp == NULL){
-		printf("ERROR: Could not open file fp in main\n");
+	fd = open(fileName, O_RDONLY);
+	if(fd == -1){
+		printf("ERROR: Could not open file fd in main\n");
 		return EXIT_FAILURE;
 	}
 	
 	int i = 0;
-	while(fgets(buf, sizeof buf, fp) != NULL){
-		args[i] = p1strdup(buf);
+	while(p1getline(fd, buf, sizeof buf) != 0){
+		buf[p1strlen(buf)] = "\0";
+		index = p1getword(buf, sizeof buf, string);
+		if(index == -1){
+			printf(" p1getword returned -1, we are at end of buf i think\n");
+		}
+		args[i] = p1strdup(string); // duplicate command name into args
+		i++;
+		args[i] = p1strdup((&buf[index])); // duplicate command arguments into args
 		i++;
 	}
 	args[i] = NULL; // sentinel so we know when out of args
@@ -44,6 +57,9 @@ int main( UNUSED int argc, char *argv[]){
 	while(args[2*i + 1] != NULL){
 		pid[i] = fork();
 		if(pid[i] == 0){
+			//test
+			printf("args: %s, %s \n", args[2*i], args[2*i + 1]);
+			//end test
 			execvp(args[2*i], args[2*i + 1]);
 		}
 		i++;
